92a6b3e3ff4b766d07da26a293314d7b95a5e112
Nguyen Van Cuwr <nguyencu14503@gmail.com>
2026-02-24 20:50:29 +0700

    video 30

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import 'dotenv/config' 
+// const express = require('express')
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('Hello World! hehe')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
